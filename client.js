6936fcf6c7165e1014d6844975944895921938a7
Juha Syrjälä <juha.syrjala@gmail.com>
2011-08-05 11:12:11 +0300

    mediawiki search api

---
+++ client.js
@@ -0,0 +1,15 @@
+require.paths.unshift('.')
+var url = require('url')
+var winston = require('winston');
+
+var client = require('mediawiki-search-api')
+// http://en.wikipedia.org/w/api.php
+
+function emptyCallback( error, result) {};
+function logCallback(error, result) { 
+	if (error) { console.log(error) }
+	if (result) { console.log(result) }
+}
+	
+var results = client.search('smash', logCallback)
+
---
+++ mediawiki-search-api.js
@@ -0,0 +1,83 @@
+var http = require('http')
+var url = require('url')
+var _ = require('underscore')
+var querystring = require('querystring')
+var defaultApiUrl = url.parse('http://en.wikipedia.org/w/api.php')
+
+// http://www.mediawiki.org/wiki/API:Opensearch
+function search(query, callback, limit, mediaWikiApiUrl) {
+	var params = [] 
+	var apiUrl = mediaWikiApiUrl || defaultApiUrl
+
+	var queryParams = {
+		action: 'opensearch',
+		search: query,
+		limit: (limit || 3),
+		format: 'json'
+	}
+	var queryString = querystring.stringify(queryParams)
+	try {
+		makeQuery(apiUrl, queryString, function(data) {
+			var results = data[1]
+			var links = _(results).map(function( pagename) { 
+					return {'page' : pagename, 'url' : linkify(apiUrl, pagename) }
+				})
+			callback(null, links)
+		}, function(error) {
+			callback(error)
+		});
+	} catch (e) {
+		callback(e)
+	}
+}
+
+function linkify(apiUrl, pageName) {
+	var path = '/wiki/' + pageName.replace(/ /g, '_')
+	var port = ''
+	if(apiUrl.port) {
+		port = ':' + port;
+	}
+	var link = url.parse(apiUrl.protocol + '//' + apiUrl.hostname + port + path)
+	return link
+}
+
+function QueryFailedException(statusCode) {
+	this.statusCode = statusCode
+}
+
+function getPort(url) {
+	if(url.port) {
+		return port
+	}
+	if(url.protocol == 'https:') {
+		return 443
+	}
+	return 80
+}
+
+function makeQuery(apiUrl, query, onSuccess, onError) {
+	var path = apiUrl.pathname + '?' + query
+	var requestParams = {
+		host: apiUrl.host,
+		port: getPort(apiUrl),
+		path: path,
+		method: 'GET',
+		headers: {'User-agent': 'Node.js WikiMedia API/0.1' }
+	}
+	var req = http.request(requestParams, function(resp) {
+		if(resp.statusCode == 200) {
+			resp.on('data', function(data) {
+				onSuccess(JSON.parse(data))
+			})
+		} else {
+			onError( new QueryFailedException(resp.statusCode))
+		}
+	});
+	
+	req.on('error', function(e) {
+		onError(e)
+	});
+	req.end();
+}
+
+exports.search = search
